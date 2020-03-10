70ea85818a4cc4685a64cfeed7605b0037f7fb25
Lucas Barros Bonine <lucasbonine@live.com>
2020-03-09 21:29:43 -0300

    Controller Empresa

---
+++ src/main/java/com/lucas/pontointeligente/api/dto/EmpresaDto.java
@@ -0,0 +1,63 @@
+package com.lucas.pontointeligente.api.dto;
+
+import java.util.Date;
+
+public class EmpresaDto {
+	
+	
+	private Long id;
+	private String razaoSocial;
+	private String cnpj;
+	private Date dataCriacao;
+	
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+
+	public EmpresaDto() {
+		
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	@Override
+	public String toString() {
+		return "EmpresaDto [id=" + id + ", razaoSocial=" + razaoSocial + ", cnpj=" + cnpj + "]";
+	}
+	
+
+}
